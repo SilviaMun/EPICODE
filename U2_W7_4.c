2f7679e46419d2c8323e543b0cf8c042c37aaa59
SilviaMun <132658800+SilviaMun@users.noreply.github.com>
2023-06-15 15:12:20 +0200

    Update U2_W7_4.c

--- U2_W7_4.c
+++ U2_W7_4.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
 int main() {
-    char buffer[10];
-    printf("Type username:\n");
-    scanf("%s", buffer);
-    printf("\nYour username is: %s\n", buffer);
+    char buffer[30];
+    printf("Come ti chiami?\n");
+    fgets(buffer, 30, stdin); //fgets al posto di scanf per evitare buffer overflow
     
-    // Segmentation fault
-    buffer[30] = 'A';
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0'; //rimuove newline input di fgets
+    }
+    
+    printf("Il tuo nome: %s\n", buffer);
+    printf("Indirizzo di memoria buffer: %p\n", (void*)buffer); //formatta indirizzo memoria
     
     return 0;
 }
+
