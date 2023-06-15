3294cdd190cebc681531c7eb6cb14ea11a2ffe8d
SilviaMun <132658800+SilviaMun@users.noreply.github.com>
2023-06-15 09:05:58 +0200

    Create U2_W7_4.c

---
+++ U2_W7_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char buffer[10];
+    printf("Type username:\n");
+    scanf("%s", buffer);
+    printf("\nYour username is: %s\n", buffer);
+    
+    // Segmentation fault
+    buffer[20] = 'A';
+    
+    return 0;
+}
